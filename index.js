ec253c176138395468fae82659393a9b41efc874
Brian Tannous <Brian@BrianTannous.com>
2021-05-24 14:35:02 -0500

    nodejs-frontend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const setupProxy = require('./src/setupProxy');
+const path = require('path')
+const app = express()
+
+
+// serving react app
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+setupProxy(app);
+
+
+
+var port = '8080'
+if (process.env.FRONTEND_PORT) {
+    port = process.env.FRONTEND_PORT;
+}
+
+app.listen(port)
+
---
+++ src/components/PostList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Post from './Post'
+import Container from 'react-bootstrap/Container'
+
+class PostList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: null,
+    }
+    this.updatePosts = this.updatePosts.bind(this)
+  }
+
+  componentDidMount() {
+    fetch('posts')
+      .then(response => response.json())
+      .then(data => this.updatePosts(data))
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.getPosts()}
+      </Container>
+    )
+  }
+
+  updatePosts(posts) {
+    if (Array.isArray(posts)) {
+      this.setState({
+        posts: posts
+      })
+    }
+  }
+
+  getPosts() {
+    if (this.state.posts == null) {
+      return <div> waiting </div>
+    } else {
+      return this.state.posts.map((post) =>
+        <Post id={post.id} title={post.title} content={post.content} timestamp={post.timestamp} key={post.timestamp}/>
+      )
+    }
+  }
+}
+
+export default PostList
